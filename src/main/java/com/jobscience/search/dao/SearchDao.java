4e146f6ebcd61cc4e915cf125316fa3ad1e9a094
wangxuwei <wangxuwei84@gmail.com>
2013-05-29 18:46:17 +0800

    #5 Add extended keywords with the ts2__skills,... tables

--- src/main/java/com/jobscience/search/dao/SearchDao.java
+++ src/main/java/com/jobscience/search/dao/SearchDao.java
@@ -3,21 +3,24 @@ package com.jobscience.search.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
 import com.google.common.base.Throwables;
 import com.jobscience.search.db.DBHelper;
 
 @Singleton
 public class SearchDao {
 
-//    static private String QUERY_SELECT = "select \"Name\", \"id\", \"Title\"" + " from contact where resume_tsv @@ to_tsquery(?) or \"Title\" ilike ? limit 30";
-//
-//    static private String QUERY_COUNT  = "select count (id)" + " from contact where resume_tsv @@ to_tsquery(?) or \"Title\" ilike ?";
+    static private String QUERY_SELECT = "select distinct a.\"Name\", a.\"id\", a.\"Title\"" + " from contact a ";
+
+    static private String QUERY_COUNT  = "select count (distinct a.id)" + " from contact a ";
 
     @Inject
     private DBHelper      dbHelper;
@@ -46,74 +49,135 @@ public class SearchDao {
         return searchResult;
     }
 
+    /**
+     * @param searchValues
+     * @param searchMode
+     * @return SearchStatements
+     */
     private SearchStatements buildSearchStatements(Connection con, Map<String, String> searchValues,SearchMode searchMode,
                                                    Integer pageIdx, Integer pageSize) {
         SearchStatements ss = new SearchStatements();
-        int offset = (pageIdx -1)* pageSize;
-
-        // build the statement
-        ss.queryStmt = dbHelper.prepareStatement(con,buildSql(searchValues, searchMode, offset, pageSize));
-        ss.countStmt = dbHelper.prepareStatement(con,buildSql(searchValues, searchMode, -1,-1));
-
-       /* // build the values
-        String search = searchValues.get("search");
-
-        String searchILike = search;
-        if (!search.contains("%")) {
-            searchILike = "%" + search + "%";
+        if(pageIdx < 0){
+            pageIdx = 0;
         }
+        int offset = (pageIdx -1) * pageSize;
 
-        String searchTq = Joiner.on(" & ").join(Splitter.on(" ").omitEmptyStrings().split(search));*/
-        ss.values = new Object[] {};
+        StringBuilder querySql = new StringBuilder();
+        StringBuilder countSql = new StringBuilder();
+        StringBuilder joinTables = new StringBuilder();
+        StringBuilder conditions = new StringBuilder();
+        List values = new ArrayList();
+        
+        boolean hasCondition = false;
+        
+        querySql.append(QUERY_SELECT);
+        countSql.append(QUERY_COUNT);
+        
+        if(searchValues!=null){
+            if(searchMode == SearchMode.KEYWORD){
+                if (searchValues.get("search") != null && !"".equals(searchValues.get("search"))) {
+                    hasCondition = true;
+                    String value = searchValues.get("search");
+                    conditions.append(" and ( a.\"Name\" ilike ? or a.\"Title\" ilike ?) ");
+                    if(!value.contains("%")){
+                        value = "%" + value + "%";
+                    }
+                    values.add(value);
+                    values.add(value);
+                }
+
+                if (searchValues.get("FirstName") != null && !"".equals(searchValues.get("FirstName"))) {
+                    hasCondition = true;
+                    String value = searchValues.get("FirstName");
+                    conditions.append(" and a.\"FirstName\" ilike ? ");
+                    if(!value.contains("%")){
+                        value = "%" + value + "%";
+                    }
+                    values.add(value);
+                }
+
+                if (searchValues.get("LastName") != null && !"".equals(searchValues.get("LastName"))) {
+                    hasCondition = true;
+                    String value = searchValues.get("LastName");
+                    conditions.append(" and a.\"LastName\" ilike ? ");
+                    if(!value.contains("%")){
+                        value = "%" + value + "%";
+                    }
+                    values.add(value);
+                }
+
+                if (searchValues.get("Skill") != null && !"".equals(searchValues.get("Skill"))) {
+                    hasCondition = true;
+                    String value = searchValues.get("Skill");
+                    joinTables.append(" inner join ts2__skill__c b on a.\"sfId\" = b.\"ts2__Contact__c\" ");
+                    conditions.append(" and b.\"ts2__Skill_Name__c\" ilike ? ");
+                    if(!value.contains("%")){
+                        value = "%" + value + "%";
+                    }
+                    values.add(value);
+                }
+
+                if (searchValues.get("Employment") != null && !"".equals(searchValues.get("Employment"))) {
+                    hasCondition = true;
+                    String value = searchValues.get("Employment");
+                    joinTables.append(" inner join ts2__employment_history__c c on a.\"sfId\" = c.\"ts2__Contact__c\" ");
+                    conditions.append(" and ( c.\"ts2__Name__c\" ilike ? or c.\"ts2__Job_Title__c\" ilike ?) ");
+                    if(!value.contains("%")){
+                        value = "%" + value + "%";
+                    }
+                    values.add(value);
+                    values.add(value);
+                }
+
+                if (searchValues.get("Education") != null && !"".equals(searchValues.get("Education"))) {
+                    hasCondition = true;
+                    String value = searchValues.get("Education");
+                    joinTables.append(" inner join ts2__education_history__c d on a.\"sfId\" = d.\"ts2__Contact__c\" ");
+                    conditions.append(" and d.\"ts2__Name__c\" ilike ? ");
+                    if(!value.contains("%")){
+                        value = "%" + value + "%";
+                    }
+                    values.add(value);
+                }
+                
+            }else if(searchMode == SearchMode.SIMPLE){
+                hasCondition = true;
+                String value = searchValues.get("search");
+                String searchTsq = Joiner.on(" & ").join(Splitter.on(" ").omitEmptyStrings().split(value));
+                String searchILike = value;
+                if (!searchILike.contains("%")) {
+                    searchILike = "%" + value + "%";
+                }
+                conditions.append(" and (a.resume_tsv @@ to_tsquery(?) or a.\"Title\" ilike ? or a.\"Name\" ilike ? )");
+                values.add(searchTsq);
+                values.add(searchILike);
+                values.add(searchILike);
+            }
+        }
+        
+        querySql.append(joinTables);
+        countSql.append(joinTables);
+        
+        if(hasCondition){
+            String whereStr = " where 1=1 ";
+            querySql.append(whereStr);
+            countSql.append(whereStr);
+        }
+        
+        querySql.append(conditions);
+        countSql.append(conditions);
+        
+        querySql.append(" offset ").append(offset).append(" limit ").append(pageSize);
+        
+        System.out.println(querySql);
+        // build the statement
+        ss.queryStmt = dbHelper.prepareStatement(con,querySql.toString());
+        ss.countStmt = dbHelper.prepareStatement(con,countSql.toString());
+        ss.values = values.toArray();
 
         return ss;
     }
     
-    /**
-     * @param searchValues
-     * @param searchMode
-     * @param offset -1, for count 0 for list,1 for count
-     * @param limit
-     * @return
-     */
-    public String buildSql( Map<String, String> searchValues,SearchMode searchMode,int offset, int limit){
-    	StringBuffer sb = new StringBuffer();
-    	if(offset>=0){
-    		sb.append("select \"Name\", \"id\", \"Title\"" + " from contact where 1=1 ");
-    	}else{
-    		sb.append("select count(id) from contact where 1=1 ");
-    	}
-    	if(searchValues!=null){
-	    	if(searchMode == SearchMode.KEYWORD){
-	    		for(String key:searchValues.keySet()){
-	    			if("search".equals(key)&&searchValues.get(key)!=null&&!"".equals(searchValues.get(key))){
-	    				sb.append(" and ( \"Name\" ilike '%"+searchValues.get(key)+"%' or \"Title\" ilike '%"+searchValues.get(key)+"%')");
-	    				continue;
-	    			}
-	    			sb.append(" and \""+covertToColumnName(key)+"\" ilike '%"+searchValues.get(key)+"%'");
-	    		}
-	    	}else if(searchMode == SearchMode.SIMPLE){
-	    		sb.append(" and resume_tsv @@ to_tsquery('"+searchValues.get("search")+"') or \"Title\" ilike '%"+searchValues.get("search")+"%'");
-	    		 sb.append("  or \"Name\" ilike '%"+searchValues.get("search")+"%'");
-	    	}
-    	}
-    	if(offset>=0){
-    		sb.append(" offset ").append(offset).append(" limit ").append(limit);
-    	}
-    	
-    	System.out.println(sb);
-    	return sb.toString();
-    }
-    
-    private String covertToColumnName(String src){
-    	if(src.toLowerCase().equals("firstname")){
-    		return "FirstName";
-    	}else if (src.toLowerCase().equals("lastname")){
-    		return "LastName";
-    	}else{
-    		return src.substring(0,1).toUpperCase()+src.substring(1).toLowerCase();
-    	}
-    }
 }
 
 
