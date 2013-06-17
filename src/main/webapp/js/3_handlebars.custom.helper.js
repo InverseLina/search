917ffb4621b91d7a4df701b57a91666d8e954724
flymyao <flymyao@gmail.com>
2013-06-17 14:37:52 +0800

    #94 when company/skill/education is empty string, do not diplay it

--- src/main/webapp/js/3_handlebars.custom.helper.js
+++ src/main/webapp/js/3_handlebars.custom.helper.js
@@ -107,4 +107,26 @@
         }
         return false;
     }
+    
+    /**
+     * remove the continues comma,like:
+     * ",,,,c1,,,,"==>"c1"
+     * ",,,,,,,,,,"==>""
+     * ",,,c1,,,,,c2"==>"c1,c2"
+     */
+    Handlebars.registerHelper('rmComma', function(src, options) {
+    	if(src){
+    		src = src+"";
+    	}else{
+    		 return new Handlebars.SafeString("");
+    	}
+    	src = src.replace(/\,{2,}/g,",");
+    	if(src.length>0&&src.substring(0,1)==","){
+    		src = src.substring(1);
+    	}
+    	if(src.length>0&&src.substring(src.length-1)==","){
+    		src = src.substring(0,src.length-1);
+    	}
+        return new Handlebars.SafeString(src);
+    });
 })(jQuery);
