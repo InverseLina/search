6b6d27d3766028cf709f5b4f89181ea64d5080e5
wangxuwei <wangxuwei84@gmail.com>
2013-06-17 17:37:50 +0800

    #96 Code clean up, remove AdvancedSearch View

--- src/main/webapp/js/SideNav.js
+++ src/main/webapp/js/SideNav.js
@@ -15,47 +15,6 @@
 		},
 		
 		events: {
-			"click; h2": function(event){
-				var view = this;
-				var $e = view.$el;
-				var $newSec = $(event.target).parent(".sec");
-                var oldMode = this.searchMode;
-				this.searchMode = $newSec.attr("data-search-mode");
-
-				var $oldSec = view.$el.find(".sec.sel");
-
-				$oldSec.removeClass("sel");
-				
-				$newSec.addClass("sel");		
-				if(this.searchMode=="advanced"){
-                    if ($e.find(".AdvancedSearch").size() == 0) {
-                        $e.find("div.sec .advanced").html(render("SideNav-advanced-loading"));
-                        var companyLimit = app.preference.get("company",6);
-                        var educationLimit = app.preference.get("education",6);
-                        var skillLimit = app.preference.get("skill",6);
-                        searchDao.getAdvancedMenu({companyLimit:companyLimit,educationLimit:educationLimit,skillLimit:skillLimit}).always(function (result) {
-                             brite.display("AdvancedSearch", $("div.advanced", $newSec), result);
-                       });
-                    }
-                }
-                var mainView = view.$el.bView("MainView");
-                if(this.searchMode != oldMode){
-                    var query = mainView.contentView.$el.find("input.search-query").val();
-                    if (/^\s*[^\s].+[^\s]\s*$/.test(query)) {
-                        view.$el.trigger("DO_SEARCH");
-                    }else{
-                        if(this.searchMode == 'simple'){
-                            mainView.contentView.empty();
-                        } else{
-                           if(!isEmptyQuery(this.getSearchValues())){
-                               view.$el.trigger("DO_SEARCH");
-                            }else{
-                               mainView.contentView.empty();
-                           }
-                        }
-                    }
-                }
-            },
 			"keypress; input[type='text']": function(event){
 				if (event.which === 13){
 					this.$el.trigger("DO_SEARCH");
