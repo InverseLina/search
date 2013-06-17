acbdab7c0d14787383d2f8cbae31f4d057f4c9d6
wangxuwei <wangxuwei84@gmail.com>
2013-06-17 14:36:14 +0800

    #81 Some UI issues in the side panel

--- src/main/webapp/js/SideSection.js
+++ src/main/webapp/js/SideSection.js
@@ -23,15 +23,14 @@
 
                       if(component.$el.bView(view.name) == view){
                         view.subComponent = component;
-                        console.log(view.subComponent);
                         if(!$.isEmptyObject(view.values)){
                             view.subComponent.updateSearchValues(view.values);
                         }
                       }
                     });
-                    view.$el.find(".open").removeClass("icon-play").addClass("icon-eject")
+                    view.$el.find(".open").removeClass("icon-chevron-right").addClass("icon-chevron-down");
                 }else {
-                    view.$el.find(".open").removeClass("icon-eject").addClass("icon-play");
+                    view.$el.find(".open").removeClass("icon-chevron-down").addClass("icon-chevron-right");
                     if(!$.isEmptyObject(view.values)){
                         view.$el.find(".content").html(render("SideSection-close", {value: formatDisplay(view.values)}))
                     }
@@ -48,6 +47,7 @@
             },
             events: {
                 "btap; .clear":function(event){
+                    event.stopPropagation();
                     var view = $(event.currentTarget).bView();
                     if(view.status == "open"){
                         view.subComponent.$el.trigger("clear");
@@ -58,7 +58,7 @@
                     app.preference.store(view.cname + ".values", null);
                     view.$el.trigger("DO_SEARCH");
                 },
-                "btap; .open":function(event){
+                "btap; .section-banner":function(event){
                     var view = $(event.currentTarget).bView();
                     if(view.status=="open"){
                         //close
@@ -73,25 +73,26 @@
                             app.preference.store(view.cname + ".values", JSON.stringify(values));
                             view.$el.find(".content").html(render("SideSection-close", {value: formatDisplay(values)}))
                         }
+                        view.$el.find(".open").removeClass("icon-chevron-down").addClass("icon-chevron-right");
                     }else{
                         //open
                         view.values = JSON.parse(app.preference.get(view.cname + ".values", ""));
                         view.status = "open";
                         app.preference.store(view.cname + ".status", "open");
                         if( view.$el.find("div."+view.cname).length>0){
-                    		view.$el.find("div."+view.cname).show();
-                    		view.$el.find("span.not-open").remove();
-                    	}else{
-	                        brite.display(view.cname , view.$el.find(".content")).done(function(component){
-	                            if(component.$el.bView(view.name) == view){
-	                                view.subComponent = component;
-	                                if(!$.isEmptyObject(view.values)){
-	                                    view.subComponent.updateSearchValues(view.values);
-	                                }
-	                            }
-	                        });
-                    	}
-                        view.$el.find(".open").removeClass("icon-chevron-right").addClass("icon-chevron-down")
+                      		view.$el.find("div."+view.cname).show();
+                      		view.$el.find("span.not-open").remove();
+                      	}else{
+  	                        brite.display(view.cname , view.$el.find(".content")).done(function(component){
+  	                            if(component.$el.bView(view.name) == view){
+  	                                view.subComponent = component;
+  	                                if(!$.isEmptyObject(view.values)){
+  	                                    view.subComponent.updateSearchValues(view.values);
+  	                                }
+  	                            }
+  	                        });
+                      	}
+                        view.$el.find(".open").removeClass("icon-chevron-right").addClass("icon-chevron-down");
                     }
                 }
             },
