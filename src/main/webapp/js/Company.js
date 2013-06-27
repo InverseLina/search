3ea1beec638dfc1457185ea1976f7cce5d199b3e
woofgl <woofgl@gmail.com>
2013-06-27 11:19:13 +0800

    #148 Use prototype way for Company view events binding

--- src/main/webapp/js/Company.js
+++ src/main/webapp/js/Company.js
@@ -8,17 +8,15 @@
 (function ($) {
     function CompanyView(){
       this.constructor._super.constructor.call(this,"company","companies");
-        
-      this.events = {
-        "change; input[name='curCompany']" : function(event) {
-          var view = this;
-          view.$el.trigger("DO_SEARCH");
-        }
-      };
-
-      this.events = $.extend(this.events, app.sidesection.BaseSideAdvanced.prototype.events);
     }
 
+    CompanyView.prototype.events = {
+        "change; input[name='curCompany']" : function(event) {
+            var view = this;
+            view.$el.trigger("DO_SEARCH");
+        }
+    };
+    CompanyView.prototype.events = $.extend(CompanyView.prototype.events, app.sidesection.BaseSideAdvanced.prototype.events);
 
     brite.inherit(CompanyView,app.sidesection.BaseSideAdvanced);
 
