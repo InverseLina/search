86875d3cc5370d358be15a5941853d722561f8a4
woofgl <woofgl@gmail.com>
2013-06-28 10:49:12 +0800

    #148 Use prototype way for Company view events binding

--- src/main/webapp/js/Company.js
+++ src/main/webapp/js/Company.js
@@ -10,6 +10,8 @@
       this.constructor._super.constructor.call(this,"company","companies");
     }
 
+    brite.inherit(CompanyView,app.sidesection.BaseSideAdvanced);
+
     CompanyView.prototype.events = {
         "change; input[name='curCompany']" : function(event) {
             var view = this;
@@ -18,7 +20,7 @@
     };
     CompanyView.prototype.events = $.extend(CompanyView.prototype.events, app.sidesection.BaseSideAdvanced.prototype.events);
 
-    brite.inherit(CompanyView,app.sidesection.BaseSideAdvanced);
+
 
     brite.registerView("Company", {emptyParent: true},function(){
       return new CompanyView();
