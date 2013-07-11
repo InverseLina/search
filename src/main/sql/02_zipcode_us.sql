4d54d35f66ecd6f21e7db65fd8b3c10cb6f4ccc3
flymyao <flymyao@gmail.com>
2013-07-11 16:03:06 +0800

    #185 Create table and indexes for zip code

---
+++ src/main/sql/02_zipcode_us.sql
@@ -0,0 +1,13 @@
+DROP TABLE if EXISTS zipcode_us;
+
+CREATE TABLE zipcode_us
+(
+  zip character(5) NOT NULL,
+  city character varying(64),
+  state character(2),
+  latitude double precision,
+  longitude double precision,
+  timezone integer,
+  dst integer,
+  CONSTRAINT zcta_pkey PRIMARY KEY (zip)
+);
