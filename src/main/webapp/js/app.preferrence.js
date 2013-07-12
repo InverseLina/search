89130705b3c51ec6535fc32dc2c168d8fcb61940
flymyao <flymyao@gmail.com>
2013-07-12 10:10:48 +0800

    #189 ERROR: column 'createdate' does not exist

--- src/main/webapp/js/app.preferrence.js
+++ src/main/webapp/js/app.preferrence.js
@@ -18,7 +18,7 @@ var app = app || {};
     var displayColumns = [{name:"id", display:"ID"},{name:"name", display:"Name"},{name:"CreatedDate",display:"Create Date"}
         ,{name:"title",display:"Title"}, {name:"company",display:"Company"}
         ,{name:"skill",display:"Skill"},{name:"education",display:"Education"}];
-    var defaultColumns = "id,name,createddate,title";
+    var defaultColumns = "id,name,CreatedDate,title";
     var defaultSectionOpen = {
         ContactInfo: 'close',
         Company: 'open',
