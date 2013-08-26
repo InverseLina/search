1cbfbcd18a946be5ffdbc1849e98584e5e2857b5
woofgl <woofgl@gmail.com>
2013-08-26 15:28:51 +0800

    #301 The Popup position changed.

--- src/main/webapp/js/ContentView.js
+++ src/main/webapp/js/ContentView.js
@@ -60,11 +60,12 @@
     	  var view = this;
     	  //$("[data-b-view^='Filter']",view.$el).bRemove();
     	  var $th = $(event.currentTarget);
-    	  var position = {top:$th.get(0).offsetTop+$th.height(),left:$th.get(0).offsetLeft+$th.width()/2-175};
+    	  var position = {top:$th.get(0).offsetTop+$th.height() + 40,left:$th.get(0).offsetLeft+$th.width()/2-175};
 //          console.log(position);
           if(position.left <  20 ){
               position.left = 20;
           }
+
     	  var type = $th.attr("data-column"); 
     	  var qName = "q_{0}s".format(type);
 
