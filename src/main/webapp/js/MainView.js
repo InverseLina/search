d0e3e22da065ccd94c838bf676a0473b70f8ddbf
woofgl <woofgl@gmail.com>
2013-09-10 17:38:57 +0800

    #357 The page selector does wrong

--- src/main/webapp/js/MainView.js
+++ src/main/webapp/js/MainView.js
@@ -115,75 +115,16 @@
 
     function doSearch(opts) {
         var view = this;
-        /*var valCount = 0;
-        var isValid = true;
-        var contentSearchValues = view.contentView.getSearchValues();
 
-        if(contentSearchValues.sort){
-            opts = $.extend(opts || {},contentSearchValues.sort);
-            delete contentSearchValues.sort;
-        }
-
-        var navContectSearchValues = {};
-        if(view.sideNav) {
-           navContectSearchValues = view.sideNav.getSearchValues();
-        }
-        var searchValues = $.extend({},contentSearchValues ,navContectSearchValues );
-        // just add the "q_"
-        var qParams = {};
-        
-        if(searchValues["radius"]&&!/^\s*\d+(\.\d+)?$/.test(searchValues["radius"])){
-       	 view.contentView.showErrorMessage("Wrong Search Query", "radius must be numberic");
-            view.$el.trigger("NO_SEARCH");
-            return false;
-       }
-
-        var errorTxt = [];
-        $.each(searchValues, function (key, val) {
-            if(!isValid) return;
-            if(/Errors$/g.test(key)){
-                errorTxt.push(val.join(","));
-                isValid = false;
-            }
-            if(!/^\s*$/.test(val)){
-                if(/^\s*[^\s].+[^\s]\s*$/.test(val)||/.*(State).*//*.test(key)){
-                    qParams["q_" + key] = $.trim(val);
-                }else{
-                	if(key=="radius"){
-                	  qParams["q_" + key] = $.trim(val);
-                	}else{
-                      isValid = false;
-                	}
-                }
-                valCount++;
-            }
-        });
-        
-        //if there has no any search text
-        if(valCount == 0 && isValid){
-            view.contentView.empty();
-            view.$el.trigger("NO_SEARCH");
-        	return false;
-        }
-        if(errorTxt.length > 0) {
-            view.contentView.showErrorMessage("Wrong Search Query", errorTxt.join(","));
-            view.$el.trigger("NO_SEARCH");
-            return false;
-        }
-        //if has some search text,but less than 3 letters
-        if(!isValid){
-            view.contentView.showErrorMessage("Wrong Search Query", "Search query needs to be at least 3 character long");
-            view.$el.trigger("NO_SEARCH");
-            return false;
-        }*/
-
-        var callback = function(){
+        var callback = function(pageIdx, pageSize){
             view.contentView.loading();
             restoreSearchParam.call(view);
+            var searchParameter = app.ParamsControl.getParamsForSearch();
 //            qParams.pageIndex = pageIdx||qParams.pageIndex;
 //            qParams.pageSize =  pageSize||qParams.pageSize;
 //            var qParams=view.getSearchValues();
-            searchDao.search(app.ParamsControl.getParamsForSearch()).always(function (result) {
+            searchParameter.pageIndex = pageIdx||1;
+            searchDao.search(searchParameter).always(function (result) {
 	            result.callback = callback;
 	            view.$el.trigger("SEARCH_RESULT_CHANGE", result);
             });
