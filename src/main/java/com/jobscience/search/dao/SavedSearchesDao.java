be26f5aa46408df691627eb7bccb44c2f6a9d24c
woofgl <woofgl@gmail.com>
2013-09-14 12:16:09 +0800

    #360 Create a SavedSearchesDao.

---
+++ src/main/java/com/jobscience/search/dao/SavedSearchesDao.java
@@ -0,0 +1,44 @@
+package com.jobscience.search.dao;
+
+
+import com.google.inject.Singleton;
+import com.jobscience.search.CurrentOrgHolder;
+import com.jobscience.search.db.DBHelper;
+
+import javax.inject.Inject;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Map;
+
+@Singleton
+public class SavedSearchesDao {
+    public static final String getSql = "select id, search, name, " +
+            "   case when create_date > COALESCE(update_date, timestamp '1970-1-1 00:00:01') then create_date " +
+            "   else update_date " +
+            "   end as new_date from savedsearches order by new_date desc offset ? limit ? ";
+    public static final String getByNameSql = "select 1 from savedsearches where name = ?";
+    public static final String deleteSql = "delete from savedsearches where id = ?";
+    public static final String insertSql = "INSERT INTO savedsearches(create_date,name, search)  VALUES (?,?, ?);";
+    public static final String updateSql = "UPDATE savedsearches SET   update_date=?, search=?  WHERE name = ?";
+    @Inject
+    private DBHelper dbHelper;
+    @Inject
+    private CurrentOrgHolder orgHolder;
+
+    public List<Map> getSavedSearches(int offset, int limit) {
+        return dbHelper.executeQuery(orgHolder.getOrgName(), getSql, offset, limit);
+    }
+
+    public void saveSavedSearches(String name, String content){
+        List<Map> list = dbHelper.executeQuery(getByNameSql, name);
+        if(list.size()==0){
+            dbHelper.executeUpdate(orgHolder.getOrgName(), insertSql, new Timestamp(System.currentTimeMillis()), name, content);
+        }else{
+            dbHelper.executeUpdate(orgHolder.getOrgName(),updateSql, new Timestamp(System.currentTimeMillis()), content, name);
+        }
+    }
+
+    public void deleteSavedSearches(Long id){
+        dbHelper.executeUpdate(orgHolder.getOrgName(),deleteSql, id);
+    }
+}
--- src/main/java/com/jobscience/search/web/SavedSearchesWebHandlers.java
+++ src/main/java/com/jobscience/search/web/SavedSearchesWebHandlers.java
@@ -3,33 +3,19 @@ package com.jobscience.search.web;
 import com.britesnow.snow.web.param.annotation.WebParam;
 import com.britesnow.snow.web.rest.annotation.WebGet;
 import com.britesnow.snow.web.rest.annotation.WebPost;
-import com.jobscience.search.CurrentOrgHolder;
-import com.jobscience.search.db.DBHelper;
+import com.jobscience.search.dao.SavedSearchesDao;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
 
 @Singleton
 public class SavedSearchesWebHandlers {
 
-
     @Inject
-    private DBHelper dbHelper;
+    private SavedSearchesDao savedSearchesDao;
 
-    @Inject
-    private CurrentOrgHolder orgHolder;
-
-    public static final String getSql = "select id, search, name, " +
-            "   case when create_date > COALESCE(update_date, timestamp '1970-1-1 00:00:01') then create_date " +
-            "   else update_date " +
-            "   end as new_date from savedsearches order by new_date desc offset ? limit ? ";
-    public static final String getByNameSql = "select 1 from savedsearches where name = ?";
-    public static final String deleteSql = "delete from savedsearches where id = ?";
-    public static final String insertSql = "INSERT INTO savedsearches(create_date,name, search)  VALUES (?,?, ?);";
-    public static final String updateSql = "UPDATE savedsearches SET   update_date=?, search=?  WHERE name = ?";
 
 
     @WebGet("/getSavedSearches")
@@ -40,20 +26,14 @@ public class SavedSearchesWebHandlers {
         if (limit == null) {
             limit = 6;
         }
-        List<Map> map = dbHelper.executeQuery(orgHolder.getOrgName(), getSql, offset, limit);
+        List<Map> map = savedSearchesDao.getSavedSearches(offset, limit);
         return WebResponse.success(map);
     }
 
     @WebPost("/saveSavedSearches")
     public WebResponse save(@WebParam("name") String name, @WebParam("content") String content) {
         try {
-            List<Map> list = dbHelper.executeQuery(getByNameSql, name);
-            if(list.size()==0){
-                dbHelper.executeUpdate(orgHolder.getOrgName(), insertSql, new Timestamp(System.currentTimeMillis()), name, content);
-            }else{
-                dbHelper.executeUpdate(orgHolder.getOrgName(),updateSql, new Timestamp(System.currentTimeMillis()), content, name);
-            }
-
+            savedSearchesDao.saveSavedSearches(name, content);
             return WebResponse.success();
         } catch (Exception e) {
             return WebResponse.fail(e.getMessage());
@@ -62,7 +42,7 @@ public class SavedSearchesWebHandlers {
 
     @WebPost("/deleteSavedSearches")
     public WebResponse delete(@WebParam("id") Long id) {
-         dbHelper.executeUpdate(orgHolder.getOrgName(),deleteSql, id);
+        savedSearchesDao.deleteSavedSearches(id);
         return WebResponse.success();
     }
 }
