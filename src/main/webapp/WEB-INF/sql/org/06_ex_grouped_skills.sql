b1ea4b70f20ff2a958b9d08302fc15e3c2d09aac
woofgl <woofgl@gmail.com>
2013-11-13 16:33:58 +0800

    #691 Add id/pk to grouped table

--- src/main/webapp/WEB-INF/sql/org/06_ex_grouped_skills.sql
+++ src/main/webapp/WEB-INF/sql/org/06_ex_grouped_skills.sql
@@ -1,12 +1,34 @@
 -- SCRIPTS
- DROP TABLE IF EXISTS  ex_grouped_skills CASCADE;
--- SCRIPTS
-create table  ex_grouped_skills as
-	select count( ts2__contact__c) as count,   c."ts2__skill_name__c"  as name, c.ts2__rating__c as rating from
-	ts2__skill__c  c where   c."ts2__skill_name__c"  !=''  group by   c."ts2__skill_name__c", c.ts2__rating__c;
+  CREATE TABLE if not exists ex_grouped_skills
+(
+  id serial NOT NULL,
+  count bigint,
+  name character varying(150),
+  CONSTRAINT ex_grouped_skills_pkey PRIMARY KEY (id)
+)
 
 -- SCRIPTS
+DO $$
+  BEGIN
+  IF NOT EXISTS (
+      SELECT 1
+      FROM   pg_class c
+      JOIN   pg_namespace n ON n.oid = c.relnamespace
+      WHERE  c.relname = 'ex_grouped_skills_idx_name'
+      AND    n.nspname =   current_schema
+      ) THEN
+     
 	CREATE INDEX ex_grouped_skills_idx_name
 	  ON ex_grouped_skills
 	  USING btree
 	  (name COLLATE pg_catalog."default");
+  END IF;
+  END$$;  
+
+-- SCRIPTS
+
+	insert into ex_grouped_skills (count,name)
+	select count( ts2__contact__c) as count,   c."ts2__skill_name__c"  as name from 
+	ts2__skill__c  c where   c."ts2__skill_name__c"  !=''  group by   c."ts2__skill_name__c"
+
+
--- src/main/webapp/WEB-INF/sql/org/09_ex_grouped_locations.sql
+++ src/main/webapp/WEB-INF/sql/org/09_ex_grouped_locations.sql
@@ -1,30 +1,38 @@
 -- SCRIPTS
- DROP TABLE IF EXISTS  ex_grouped_locations CASCADE;
+DROP TABLE IF EXISTS  ex_grouped_locations CASCADE;
 
 -- SCRIPTS
- create table  ex_grouped_locations as
-  SELECT
-    count(C.id)  AS count , z.city as name, c.mailingpostalcode  AS zip,
-    public.earth_distance(public.ll_to_earth(z."latitude", z."longitude"),
-                          public.ll_to_earth(c."ts2__latitude__c", c."ts2__longitude__c")) / 1000 AS distance
-  FROM (SELECT
-          *
-        FROM jss_sys.zipcode_us z) z INNER JOIN PUBLIC.contact C
-      ON c.mailingpostalcode = z.zip
-  GROUP BY z.city, c."mailingpostalcode",
-    public.earth_distance(public.ll_to_earth(z."latitude", z."longitude"),
-                          public.ll_to_earth(c."ts2__latitude__c", c."ts2__longitude__c")) / 1000;
+CREATE TABLE if not exists ex_grouped_locations
+(
+  id serial NOT NULL,
+  count bigint,
+  name character varying(150),
+  CONSTRAINT ex_grouped_locations_pkey PRIMARY KEY (id)
+);
 
 -- SCRIPTS
-	CREATE INDEX ex_grouped_locations_idx_city
-	  ON ex_grouped_locations
-	  USING btree
-	  (name COLLATE pg_catalog."default");
+DO $$
+  BEGIN
+  IF NOT EXISTS (
+      SELECT 1
+      FROM   pg_class c
+      JOIN   pg_namespace n ON n.oid = c.relnamespace
+      WHERE  c.relname = 'ex_grouped_locations_idx_name'
+      AND    n.nspname =   current_schema
+      ) THEN
+
+  CREATE INDEX ex_grouped_locations_idx_name
+    ON ex_grouped_locations
+    USING btree
+    (name COLLATE pg_catalog."default");
+  END IF;
+  END$$;
+
 -- SCRIPTS
-	CREATE INDEX ex_grouped_locations_idx_zip
-	  ON ex_grouped_locations
-	  USING btree
-	  (zip COLLATE pg_catalog."default");
+
+insert into ex_grouped_locations (count,name)
+  select count(c.id), z.city from contact c inner join jss_sys.zipcode_us z on c.mailingpostalcode = z.zip
+  group by z.city
 
 
 
