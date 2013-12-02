692e2d6161c7f2c86cbbd96cba3e147ebc440da0
woofgl <woofgl@gmail.com>
2013-12-02 17:08:27 +0800

    #711 Refactoring to use josql for Label

--- src/main/java/com/jobscience/search/dao/LabelDao.java
+++ src/main/java/com/jobscience/search/dao/LabelDao.java
@@ -9,59 +9,59 @@ import com.jobscience.search.CurrentOrgHolder;
 public class LabelDao {
     
     @Inject
-    private DaoHelper dbHelper;
+    private DaoHelper daoHelper;
     @Inject
     private CurrentOrgHolder orgHolder;
     
     public Long addLabel(Long userId,String name) {
-        Long id = dbHelper.executeInsertReturnId(orgHolder.getOrgName(),
+        Long id = (Long) daoHelper.insert(orgHolder.getOrgName(),
                 "insert into label(name,user_id) values(?,?)", name, userId);
         return id;
     }
     
     public void deleteLabel(String name){
-        dbHelper.executeUpdate(orgHolder.getOrgName(),
+        daoHelper.executeUpdate(orgHolder.getOrgName(),
                 "delete from label where name=?", name);
     }
     
     public void deleteLabel(Long id){
-        dbHelper.executeUpdate(orgHolder.getOrgName(),
+        daoHelper.executeUpdate(orgHolder.getOrgName(),
                 "delete from label where id=?", id);
     }
     
     public void updateLabel(Long labelId,String name){
-        dbHelper.executeUpdate(orgHolder.getOrgName(),
-                "update label set name=? where id=?", name,labelId);
+        daoHelper.executeUpdate(orgHolder.getOrgName(),
+                "update label set name=? where id=?", name, labelId);
     }
     
     public List<Map> getLabelForUser(Long userId){
-        return dbHelper.executeQuery(orgHolder.getOrgName(),
+        return daoHelper.executeQuery(orgHolder.getOrgName(),
                 "select * from  label where user_id=?", userId);
     }
     
     public  List<Map>  getLabelForById(Long labelId){
-        return dbHelper.executeQuery(orgHolder.getOrgName(),
+        return daoHelper.executeQuery(orgHolder.getOrgName(),
                 "select * from  label where id=?", labelId);
     }
     
     public  List<Map>  getLabelByName(String name, Object userId){
-        return dbHelper.executeQuery(orgHolder.getOrgName(),
+        return daoHelper.executeQuery(orgHolder.getOrgName(),
                 "select * from  label where name=? and user_id = ? limit 1", name, userId);
     }
     
     
     public void assignLabelToContact(Long contactId,Long labelId){
-        dbHelper.executeUpdate(orgHolder.getOrgName(),
-                "insert into label_contact(label_id,contact_id) values(?,?)", labelId,contactId);
+        daoHelper.executeUpdate(orgHolder.getOrgName(),
+                "insert into label_contact(label_id,contact_id) values(?,?)", labelId, contactId);
     }
     
     public void unAssignLabelFromContact(Long contactId,Long labelId){
-        dbHelper.executeUpdate(orgHolder.getOrgName(),
-                "delete from label_contact where label_id=? and contact_id=?", labelId,contactId);
+        daoHelper.executeUpdate(orgHolder.getOrgName(),
+                "delete from label_contact where label_id=? and contact_id=?", labelId, contactId);
     }
     
     public List<Map>  getLabelStatus(String contactIds,Long labelId){
-       return  dbHelper.executeQuery(orgHolder.getOrgName(),
+       return  daoHelper.executeQuery(orgHolder.getOrgName(),
                 "select c.id, case  when l.label_id is null then false else true end haslabel from" +
                 " (select id from contact where id in ("+contactIds+"))" +
                 " c left join label_contact l on c.id=l.contact_id and  l.label_id=?", labelId);
