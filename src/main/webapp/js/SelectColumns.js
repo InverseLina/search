563431ae6f9a98aaeb6c9ee50842a431d6199621
woofgl <woofgl@gmail.com>
2014-01-21 17:50:24 +0800

    #855 impletment select column drag and drop

--- src/main/webapp/js/SelectColumns.js
+++ src/main/webapp/js/SelectColumns.js
@@ -10,7 +10,24 @@
         {
             // --------- View Interface Implement--------- //
             create: function (data, config) {
-                return render("SelectColumns", {columns:app.getSearchUiConfig(true)});
+                var i, columns = app.getSearchUiConfig(true);
+                var orders = app.getFilterOrders();
+                var colOrders = [];
+                var ids = [];
+                $.each(orders, function(idx, name){
+                    $.each(columns, function(idx, item){
+                        if(item.name == name){
+                            colOrders.push(item);
+                            ids.push(idx)
+                        }
+                    })
+                })
+                for(i = 0; i < columns.length; i++){
+                    if($.inArray(i, ids)< 0){
+                        colOrders.push(columns[i]);
+                    }
+                }
+                return render("SelectColumns", {columns:colOrders});
             },
 
             postDisplay: function (data) {
@@ -60,6 +77,70 @@
                         $checkbox.prop("checked",true);
                     }
 
+                },
+                "bdragstart; li": function (event) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    var $li = $(event.currentTarget);
+                    var view = this;
+                    var $e = view.$el;
+                    var $clone = $li.clone();
+                    $clone.attr("id","columnsClone");
+                    var pos = $li.position();
+                    $clone.css({"display": "block","position":"absolute", opacity:0.5, left: pos.left, top: pos.top,
+                        "cursor": "pointer", "width":$li.outerWidth()});
+
+                    var index = $li.index();
+                    $e.find(".columns li:nth-child("+(index + 1)+")").each(function(){
+                        $(this).addClass("holderSpace");
+                    });
+
+                    view.$el.append($clone);
+                },
+                "bdragmove; li": function (e) {
+                    var view = this;
+                    var $e = view.$el;
+                    e.stopPropagation();
+                    e.preventDefault();
+                    var $clone = view.$el.find("#columnsClone");
+                    var ppos = $clone.position();
+                    var pos = {top: ppos.top + e.bextra.deltaY, left: 10}
+                    $clone.css(pos);
+
+
+                    var $holder = $e.find(".holderSpace");
+                    $e.find(".columns li").each(function (idx, li) {
+                        var $li = $(li);
+                        var tpos = $li.offset();
+                        if (e.bextra.pageY > tpos.top && e.bextra.pageY < tpos.top + $li.outerHeight()) {
+                            if (e.bextra.pageY < tpos.top + $li.outerHeight() / 2) {
+                                $holder.insertAfter($li);
+                            } else {
+                                $holder.insertBefore($li);
+                            }
+                        }
+                    });
+                },
+                "bdragend; li":function(e){
+                    e.stopPropagation();
+                    e.preventDefault();
+                    var view = this;
+                    var $e = view.$el;
+
+                    var columns = [];
+                    $e.find(".columns li").each(function(idx, th){
+                        var $li = $(th);
+                        columns.push($li.find("input").val());
+                    });
+                    view.$el.find("#columnsClone").remove();
+
+                    $e.find(".columns li").removeClass("holderSpace");
+
+                    app.getJsonData("perf/save-user-pref", {value: JSON.stringify(columns)},"Post").done(function(){
+
+                        app.getFilterOrders(columns);
+                        view.$el.trigger("DO_SEARCH");
+                    });
                 }
 
             }
