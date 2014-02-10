ed65eef12be7db252c4359ba96d4fd3a7122b973
flymyao <flymyao@gmail.com>
2014-02-10 21:14:42 +0800

    #920 can't multiply data

--- src/main/webapp/WEB-INF/sql/org/04_multiply_data.sql
+++ src/main/webapp/WEB-INF/sql/org/04_multiply_data.sql
@@ -12,7 +12,7 @@
         else
            newcolumns:=(select string_agg(column_name,',') from information_schema.columns
            where table_schema=current_schema and table_name=tableName and column_name not in ('id','sfid','ts2__contact__c'));
-           sql:='insert into '||tableName||' (ts2__contact__c,sfid,'||newcolumns|| ')  select ('||iteration_num||'||substring(ts2__contact__c from 4)) as ts2__contact__c,('||iteration_num||'||substring(sfid from 4)) as sfid,'|| newcolumns||' from '||tableName||'   where id in (select id from contact order by id asc offset '||offsetNum||' limit '||limitNum||')'; 
+           sql:='insert into '||tableName||' (ts2__contact__c,sfid,'||newcolumns|| ')  select ('||iteration_num||'||substring(ts2__contact__c from 4)) as ts2__contact__c,('||iteration_num||'||substring(sfid from 4)) as sfid,'|| newcolumns||' from '||tableName||'   where id in (select id from '||tableName||' order by id asc offset '||offsetNum||' limit '||limitNum||')'; 
            EXECUTE  sql; 
         end IF;
            
