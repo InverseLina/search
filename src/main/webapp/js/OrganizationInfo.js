2bfa22f98d252b812c1abe5dc01cb80af925914d
woofgl <woofgl@gmail.com>
2014-03-10 17:45:28 +0800

    #998 The button "Remove Wrong indexes" is not enable after finish "Other Indexes Created" in the Web UI Setup

--- src/main/webapp/js/OrganizationInfo.js
+++ src/main/webapp/js/OrganizationInfo.js
@@ -346,8 +346,10 @@
 				}else{
 					view.$el.find(".index").prop("disabled",true).addClass("btn-success").html("Other Indexes Created");
 				}
-					view.$el.find(".index-status-bar .progress-bar-success").css("width",percentage+"%");
-					view.$el.find(".index-status-bar .db-percentage").html(data.created+"/"+data.all);
+				view.$el.find(".index-status-bar .progress-bar-success").css("width",percentage+"%");
+				view.$el.find(".index-status-bar .db-percentage").html(data.created+"/"+data.all);
+				view.$el.trigger("WRONGINDEXES");
+					
 			});
 		},
 		"CONTACTINDEXCOLUMNSSTATUS":function(){
@@ -360,8 +362,8 @@
 					view.$el.find(".contact-index").prop("disabled",true).addClass("btn-success").html("jss_contact Indexes Created");
 				}
 				percentage = data.created/3*100;
-					view.$el.find(".contact-index-status-bar .progress-bar-success").css("width",percentage+"%");
-					view.$el.find(".contact-index-status-bar .contact-index-percentage").html(data.created+"/3");
+				view.$el.find(".contact-index-status-bar .progress-bar-success").css("width",percentage+"%");
+				view.$el.find(".contact-index-status-bar .contact-index-percentage").html(data.created+"/3");
 			});
 		},
 		"click;.resume":function(event){ 
@@ -388,7 +390,7 @@
 					window.clearInterval(view.intervalId);
 				});
 				view.intervalId = window.setInterval(function(){
-							$(view.el).trigger("RESUMEINDEXSTATUS");
+					$(view.el).trigger("RESUMEINDEXSTATUS");
 				}, 3000);
 
 			}else if(status=="pause"){
