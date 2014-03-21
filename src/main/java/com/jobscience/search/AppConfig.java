91d18def70565c47e3f92556f18c4a310d4dc216
jeremychone <jeremy.chone@gmail.com>
2014-03-21 12:12:32 -0700

    . update version to 026

--- src/main/java/com/jobscience/search/AppConfig.java
+++ src/main/java/com/jobscience/search/AppConfig.java
@@ -26,7 +26,7 @@ public class AppConfig extends AbstractModule {
     @SuppressWarnings("unused")
     private static Logger log = LoggerFactory.getLogger(AppConfig.class);
 
-	public static String JSS_VERSION = "026-SNAPSHOT";
+	public static String JSS_VERSION = "026";
     
     @Override
     protected void configure() {
