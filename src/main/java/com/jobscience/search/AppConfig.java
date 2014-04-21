d1bcffce6303f7556b2ab8da4a63fc55e26c5976
jeremychone <jeremy.chone@gmail.com>
2014-04-21 14:46:44 -0700

    . update JSS_VERSION to 034

--- src/main/java/com/jobscience/search/AppConfig.java
+++ src/main/java/com/jobscience/search/AppConfig.java
@@ -27,7 +27,7 @@ public class AppConfig extends AbstractModule {
     @SuppressWarnings("unused")
     private static Logger log = LoggerFactory.getLogger(AppConfig.class);
 
-	public static String JSS_VERSION = "034-SNAPSHOT";
+	public static String JSS_VERSION = "034";
     
     @Override
     protected void configure() {
