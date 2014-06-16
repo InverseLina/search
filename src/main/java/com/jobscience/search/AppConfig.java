caae63960790e271a4ebe4db0fa9739aedff6ca6
jeremychone <jeremy.chone@gmail.com>
2014-06-16 12:25:05 -0700

    . updated JSS_VERSION to 040

--- src/main/java/com/jobscience/search/AppConfig.java
+++ src/main/java/com/jobscience/search/AppConfig.java
@@ -27,7 +27,7 @@ public class AppConfig extends AbstractModule {
     @SuppressWarnings("unused")
     private static Logger log = LoggerFactory.getLogger(AppConfig.class);
 
-	public static String JSS_VERSION = "040-SNAPSHOT";
+	public static String JSS_VERSION = "040";
     
     @Override
     protected void configure() {
