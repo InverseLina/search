5a8cae840435fad32efaf1d84c43b50bad9a2e8c
flymyao <flymyao@gmail.com>
2014-08-05 11:21:04 +0800

    .update default org schema to demo64

--- src/main/webapp/WEB-INF/sql/jss_sys/03_seed_jobscience_db.sql
+++ src/main/webapp/WEB-INF/sql/jss_sys/03_seed_jobscience_db.sql
@@ -6,6 +6,6 @@
 	    FROM  org    
 	    WHERE  name = 'JobScience'
 	    ) THEN
-	  insert into org (name, schemaname, sfid) values ('JobScience','org_jbs', null);
+	  insert into org (name, schemaname, sfid) values ('JobScience','demo64', null);
 	END IF;
 	END$$;
