7372bae74246094de92c2bac20b116c2c84349cb
flymyao <flymyao@gmail.com>
2014-08-08 20:52:33 +0800

    .fix Trigger issues

--- src/main/webapp/WEB-INF/sql/org/01_create_extra.sql
+++ src/main/webapp/WEB-INF/sql/org/01_create_extra.sql
@@ -77,145 +77,7 @@ CREATE TABLE if not exists jss_searchlog
   perffetch bigint NOT NULL DEFAULT 0,
   CONSTRAINT jss_searchlog_pkey PRIMARY KEY (id)
 );
-
--- SCRIPTS
-CREATE OR REPLACE FUNCTION update_ex_group_skills() RETURNS trigger AS $BODY$ 
-  DECLARE  c integer; 
-  declare schema_name varchar; 
-  BEGIN
-  schema_name := '"'||TG_TABLE_SCHEMA||'"';
-  EXECUTE   'set search_path to '||schema_name;
-        IF(TG_OP = 'UPDATE' OR TG_OP = 'DELETE') THEN
-          
-            c:=(select "count" from jss_grouped_skills where name = OLD.ts2__skill_name__c);
-              
-            IF (c=1) THEN 
-                DELETE FROM  jss_grouped_skills where name = OLD.ts2__skill_name__c;
-            ELSE 
-                UPDATE jss_grouped_skills set count=(c-1) where name = OLD.ts2__skill_name__c;
-            END IF;
-                
-        END IF;
-
-        IF(TG_OP = 'UPDATE' OR TG_OP = 'INSERT') THEN
-        
-	        SELECT jss_grouped_skills.count into c from jss_grouped_skills where name = NEW.ts2__skill_name__c limit 1;
-	        IF FOUND THEN
-	           UPDATE jss_grouped_skills set count=(c+1) where name = NEW.ts2__skill_name__c;
-	        ELSE
-	           INSERT into jss_grouped_skills(count,name) values(1,NEW.ts2__skill_name__c);
-	        END IF; 
-	        
-		END IF;
-
-        RETURN NEW;
-      END;  
-   $BODY$
-    LANGUAGE plpgsql;
-    
--- SCRIPTS
-  DROP trigger if exists skill_trigger on ts2__skill__c;
-  
--- SCRIPTS
-  CREATE  TRIGGER skill_trigger
-    AFTER INSERT OR UPDATE OF ts2__skill_name__c
-    ON ts2__skill__c
-    FOR EACH ROW
-    EXECUTE PROCEDURE update_ex_group_skills();
-
--- SCRIPTS
-CREATE OR REPLACE FUNCTION update_ex_group_educations() RETURNS trigger AS $BODY$ 
-  DECLARE  c integer; 
-  declare schema_name varchar; 
-  BEGIN
-  schema_name := '"'||TG_TABLE_SCHEMA||'"';
-  EXECUTE   'set search_path to '||schema_name;
-        IF(TG_OP = 'UPDATE' OR TG_OP = 'DELETE') THEN
-          
-            c:=(select "count" from jss_grouped_educations where name = OLD.ts2__name__c);
-              
-            IF (c=1) THEN 
-                DELETE FROM  jss_grouped_educations where name = OLD.ts2__name__c;
-            ELSE 
-                UPDATE jss_grouped_educations set count=(c-1) where name = OLD.ts2__name__c;
-            END IF;
-                
-        END IF;
-
-        IF(TG_OP = 'UPDATE' OR TG_OP = 'INSERT') THEN
-        
-	        SELECT jss_grouped_educations.count into c from jss_grouped_educations where name = NEW.ts2__name__c limit 1;
-	        IF FOUND THEN
-	           UPDATE jss_grouped_educations set count=(c+1) where name = NEW.ts2__name__c;
-	        ELSE
-	           INSERT into jss_grouped_educations(count,name) values(1,NEW.ts2__name__c);
-	        END IF; 
-
-		END IF;
-
-        RETURN NEW;
-      END;  
-   $BODY$
-    LANGUAGE plpgsql;
-    
--- SCRIPTS
-  DROP trigger if exists education_trigger on ts2__education_history__c;
-  
--- SCRIPTS
-  CREATE  TRIGGER education_trigger
-    AFTER INSERT OR UPDATE OF ts2__name__c
-    ON ts2__education_history__c
-    FOR EACH ROW
-    EXECUTE PROCEDURE update_ex_group_educations();
-
--- SCRIPTS
-CREATE OR REPLACE FUNCTION update_ex_group_employers() RETURNS trigger AS $BODY$  
-  DECLARE  c integer; 
-  declare schema_name varchar; 
-  BEGIN
-  schema_name := '"'||TG_TABLE_SCHEMA||'"';
-  EXECUTE   'set search_path to '||schema_name;
-        IF(TG_OP = 'UPDATE' OR TG_OP = 'DELETE') THEN
-          
-            c:=(select "count" from jss_grouped_employers where name = OLD.ts2__name__c);
-              
-            IF (c=1) THEN 
-                DELETE FROM  jss_grouped_employers where name = OLD.ts2__name__c;
-            ELSE 
-                UPDATE jss_grouped_employers set count=(c-1) where name = OLD.ts2__name__c;
-            END IF;
-                
-        END IF;
-
-		IF(TG_OP = 'UPDATE' OR TG_OP = 'INSERT') THEN
-        
-	        SELECT jss_grouped_employers.count into c from jss_grouped_employers where name = NEW.ts2__name__c limit 1;
-	        IF FOUND THEN
-	           UPDATE jss_grouped_employers set count=(c+1) where name = NEW.ts2__name__c;
-	        ELSE
-	           INSERT into jss_grouped_employers(count,name) values(1,NEW.ts2__name__c);
-	        END IF;
-	      
-		END IF;
-
-
-        RETURN NEW;
-      END;  
-   $BODY$
-    LANGUAGE plpgsql;
-    
--- SCRIPTS
-  DROP trigger if exists employer_trigger on ts2__employment_history__c;
-  
--- SCRIPTS
-  CREATE  TRIGGER employer_trigger
-    AFTER INSERT OR UPDATE OF ts2__name__c
-    ON ts2__employment_history__c
-    FOR EACH ROW
-    EXECUTE PROCEDURE update_ex_group_employers();
-
-
-
+ 
 -- SCRIPTS
 CREATE TABLE if not exists jss_pref
 (
@@ -251,62 +113,113 @@ CREATE TABLE if not exists jss_contact_jss_groupby_employers
   year double precision
 );
 
+-- SCRIPTS
+DROP FUNCTION if exists update_ex_group_skills() cascade;
 
 -- SCRIPTS
-CREATE OR REPLACE FUNCTION update_jss_contact_groupby_skills() RETURNS trigger AS $BODY$ 
-  declare schema_name varchar; 
+DROP FUNCTION if exists update_jss_contact_groupby_skills() cascade;
+
+-- SCRIPTS
+CREATE OR REPLACE FUNCTION update_skill_related_tables() RETURNS trigger AS $BODY$ 
+  DECLARE schema_name varchar; 
+  DECLARE  c integer; 
   BEGIN
   schema_name := '"'||TG_TABLE_SCHEMA||'"';
   EXECUTE   'set search_path to '||schema_name;
-
-        IF(TG_OP = 'UPDATE') THEN
-          delete from jss_contact_jss_groupby_skills t
+      IF(TG_OP = 'UPDATE' OR TG_OP = 'DELETE') THEN
+      
+           delete from jss_contact_jss_groupby_skills t
             where t.jss_contact_id = (select id from contact where sfid = OLD.ts2__contact__c) 
             and t.jss_groupby_skills_id = (select id from jss_grouped_skills where name = OLD.ts2__skill_name__c);
-        END IF;
+            
+            c:=(select "count" from jss_grouped_skills where name = OLD.ts2__skill_name__c);
+              
+            IF (c=1) THEN 
+                DELETE FROM  jss_grouped_skills where name = OLD.ts2__skill_name__c;
+            ELSE 
+                UPDATE jss_grouped_skills set count=(c-1) where name = OLD.ts2__skill_name__c;
+            END IF;
 
-        insert into jss_contact_jss_groupby_skills (jss_contact_id,jss_groupby_skills_id,rating)
-        select  c.id as jss_contact_id, gskill.id as jss_groupby_skills_id, max(skill.ts2__rating__c) from ts2__skill__c skill 
-        inner join contact c on skill.ts2__contact__c = c.sfid 
-        inner join jss_grouped_skills gskill on gskill.name = skill.ts2__skill_name__c
-        where skill.ts2__skill_name__c = NEW.ts2__skill_name__c and skill.ts2__contact__c = NEW.ts2__contact__c 
-        group by c.id, gskill.id;
+      END IF;
 
-        RETURN NEW;
+      IF(TG_OP = 'UPDATE' OR TG_OP = 'INSERT') THEN
+        
+	        SELECT jss_grouped_skills.count into c from jss_grouped_skills where name = NEW.ts2__skill_name__c limit 1;
+	        IF FOUND THEN
+	           UPDATE jss_grouped_skills set count=(c+1) where name = NEW.ts2__skill_name__c;
+	        ELSE
+	           INSERT into jss_grouped_skills(count,name) values(1,NEW.ts2__skill_name__c);
+	        END IF; 
+	        
+	  END IF;
+      
+      insert into jss_contact_jss_groupby_skills (jss_contact_id,jss_groupby_skills_id,rating)
+      select  c.id as jss_contact_id, gskill.id as jss_groupby_skills_id, max(skill.ts2__rating__c) from ts2__skill__c skill 
+      inner join contact c on skill.ts2__contact__c = c.sfid 
+      inner join jss_grouped_skills gskill on gskill.name = skill.ts2__skill_name__c
+      where skill.ts2__skill_name__c = NEW.ts2__skill_name__c and skill.ts2__contact__c = NEW.ts2__contact__c 
+      group by c.id, gskill.id;
+
+      RETURN NEW;
   		EXCEPTION 
   			WHEN unique_violation THEN
+  			raise notice 'error';
         	RETURN NEW;
       END;  
    $BODY$
     LANGUAGE plpgsql;
-
 -- SCRIPTS
   DROP trigger if exists skill_trigger_group on ts2__skill__c;
- 
- 
- 
- 
+
 -- SCRIPTS
-CREATE  TRIGGER skill_trigger_group
+  DROP trigger if exists skill_trigger on ts2__skill__c;
+  
+-- SCRIPTS
+  CREATE  TRIGGER skill_trigger
     AFTER INSERT OR UPDATE OF ts2__skill_name__c,ts2__contact__c
     ON ts2__skill__c
     FOR EACH ROW
-    EXECUTE PROCEDURE update_jss_contact_groupby_skills();
-
-
+    EXECUTE PROCEDURE update_skill_related_tables();
 
 -- SCRIPTS
-CREATE OR REPLACE FUNCTION update_jss_contact_groupby_educations() RETURNS trigger AS $BODY$ 
-  declare schema_name varchar; 
+DROP FUNCTION if exists update_ex_group_educations() cascade;
+
+-- SCRIPTS
+DROP FUNCTION if exists update_jss_contact_groupby_educations() cascade;
+
+-- SCRIPTS
+CREATE OR REPLACE FUNCTION update_educations_related_tables() RETURNS trigger AS $BODY$ 
+  DECLARE schema_name varchar; 
+  DECLARE  c integer; 
   BEGIN
   schema_name := '"'||TG_TABLE_SCHEMA||'"';
   EXECUTE   'set search_path to '||schema_name;
+      IF(TG_OP = 'UPDATE' OR TG_OP = 'DELETE') THEN
 
-        IF(TG_OP = 'UPDATE') THEN
-          delete from jss_contact_jss_groupby_educations t
+            delete from jss_contact_jss_groupby_educations t
             where t.jss_contact_id = (select id from contact where sfid = OLD.ts2__contact__c) 
-            and t.jss_groupby_educations_id = (select id from jss_grouped_educations where name = OLD.ts2__name__c);
-        END IF;
+            and t.jss_groupby_educations_id = (select id from jss_grouped_educations where name = OLD.ts2__name__c);            
+           
+            c:=(select "count" from jss_grouped_educations where name = OLD.ts2__name__c);
+              
+            IF (c=1) THEN 
+                DELETE FROM  jss_grouped_educations where name = OLD.ts2__name__c;
+            ELSE 
+                UPDATE jss_grouped_educations set count=(c-1) where name = OLD.ts2__name__c;
+            END IF;
+
+      END IF;
+
+      IF(TG_OP = 'UPDATE' OR TG_OP = 'INSERT') THEN
+        
+	        SELECT jss_grouped_educations.count into c from jss_grouped_educations where name = NEW.ts2__name__c limit 1;
+          IF FOUND THEN
+             UPDATE jss_grouped_educations set count=(c+1) where name = NEW.ts2__name__c;
+          ELSE
+             INSERT into jss_grouped_educations(count,name) values(1,NEW.ts2__name__c);
+          END IF; 
+	        
+		  END IF;
 
         insert into jss_contact_jss_groupby_educations (jss_contact_id,jss_groupby_educations_id)
         select  c.id as jss_contact_id, geducation.id as jss_groupby_educations_id from ts2__education_history__c education 
@@ -314,40 +227,68 @@ CREATE OR REPLACE FUNCTION update_jss_contact_groupby_educations() RETURNS trigg
         inner join jss_grouped_educations geducation on geducation.name = education.ts2__name__c
         where education.ts2__name__c = NEW.ts2__name__c and education.ts2__contact__c = NEW.ts2__contact__c 
         group by c.id, geducation.id;
-        
-  		RETURN NEW;
+
+      RETURN NEW;
   		EXCEPTION 
   			WHEN unique_violation THEN
+  			raise notice 'error';
         	RETURN NEW;
       END;  
    $BODY$
     LANGUAGE plpgsql;
 
-
 -- SCRIPTS
   DROP trigger if exists education_trigger_group on ts2__education_history__c;
-  
+ 
+-- SCRIPTS
+  DROP trigger if exists education_trigger on ts2__education_history__c; 
   
 -- SCRIPTS
-CREATE  TRIGGER education_trigger_group
+CREATE  TRIGGER education_trigger
     AFTER INSERT OR UPDATE OF ts2__name__c,ts2__contact__c
     ON ts2__education_history__c
     FOR EACH ROW
-    EXECUTE PROCEDURE update_jss_contact_groupby_educations();
-    
-    
+    EXECUTE PROCEDURE update_educations_related_tables();
+
 -- SCRIPTS
-CREATE OR REPLACE FUNCTION update_jss_contact_groupby_employers() RETURNS trigger AS $BODY$ 
-  declare schema_name varchar; 
+DROP FUNCTION if exists update_ex_group_employers() cascade;
+
+-- SCRIPTS
+DROP FUNCTION if exists update_jss_contact_groupby_employers() cascade;
+
+-- SCRIPTS
+CREATE OR REPLACE FUNCTION update_employers_related_tables() RETURNS trigger AS $BODY$ 
+  DECLARE schema_name varchar; 
+  DECLARE  c integer; 
   BEGIN
   schema_name := '"'||TG_TABLE_SCHEMA||'"';
   EXECUTE   'set search_path to '||schema_name;
+      IF(TG_OP = 'UPDATE' OR TG_OP = 'DELETE') THEN
 
-        IF(TG_OP = 'UPDATE') THEN
-          delete from jss_contact_jss_groupby_employers t
+            delete from jss_contact_jss_groupby_employers t
             where t.jss_contact_id = (select id from contact where sfid = OLD.ts2__contact__c) 
             and t.jss_groupby_employers_id = (select id from jss_grouped_employers where name = OLD.ts2__name__c);
-        END IF;
+                       
+            c:=(select "count" from jss_grouped_employers where name = OLD.ts2__name__c);
+              
+            IF (c=1) THEN 
+                DELETE FROM  jss_grouped_employers where name = OLD.ts2__name__c;
+            ELSE 
+                UPDATE jss_grouped_employers set count=(c-1) where name = OLD.ts2__name__c;
+            END IF;
+
+      END IF;
+
+      IF(TG_OP = 'UPDATE' OR TG_OP = 'INSERT') THEN
+        
+	        SELECT jss_grouped_employers.count into c from jss_grouped_employers where name = NEW.ts2__name__c limit 1;
+          IF FOUND THEN
+             UPDATE jss_grouped_employers set count=(c+1) where name = NEW.ts2__name__c;
+          ELSE
+             INSERT into jss_grouped_employers(count,name) values(1,NEW.ts2__name__c);
+          END IF; 
+	        
+		  END IF;
 
         insert into jss_contact_jss_groupby_employers (jss_contact_id,jss_groupby_employers_id,year) 
         select  c.id as jss_contact_id, gemployer.id as jss_groupby_employers_id, max(EXTRACT(year from age(employer.ts2__employment_end_date__c,employer.ts2__employment_start_date__c))) 
@@ -357,9 +298,10 @@ CREATE OR REPLACE FUNCTION update_jss_contact_groupby_employers() RETURNS trigge
         where employer.ts2__name__c = NEW.ts2__name__c and employer.ts2__contact__c = NEW.ts2__contact__c 
         group by c.id, gemployer.id;
 
-  		RETURN NEW;
+      RETURN NEW;
   		EXCEPTION 
   			WHEN unique_violation THEN
+  			raise notice 'error';
         	RETURN NEW;
       END;  
    $BODY$
@@ -367,13 +309,15 @@ CREATE OR REPLACE FUNCTION update_jss_contact_groupby_employers() RETURNS trigge
 
 -- SCRIPTS
   DROP trigger if exists employer_trigger_group on ts2__employment_history__c;
-  
+   
+-- SCRIPTS
+  DROP trigger if exists employer_trigger on ts2__employment_history__c; 
   
 -- SCRIPTS
-CREATE  TRIGGER employer_trigger_group
+CREATE  TRIGGER employer_trigger
     AFTER INSERT OR UPDATE OF ts2__name__c,ts2__contact__c
     ON ts2__employment_history__c
     FOR EACH ROW
-    EXECUTE PROCEDURE update_jss_contact_groupby_employers();
+    EXECUTE PROCEDURE update_employers_related_tables();
 
 
