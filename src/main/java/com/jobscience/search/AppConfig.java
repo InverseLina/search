a5f091b6890878df551c5e366f7c7960e588f264
flymyao <flymyao@gmail.com>
2014-09-05 17:29:34 +0800

    .update version to DROP-CUSTOMFIELD-02

--- src/main/java/com/jobscience/search/AppConfig.java
+++ src/main/java/com/jobscience/search/AppConfig.java
@@ -27,7 +27,7 @@ public class AppConfig extends AbstractModule {
     @SuppressWarnings("unused")
     private static Logger log = LoggerFactory.getLogger(AppConfig.class);
 
-	public static String JSS_VERSION = "DROP-CUSTOMFIELDS-01";
+	public static String JSS_VERSION = "DROP-CUSTOMFIELD-02";
     
     @Override
     protected void configure() {
