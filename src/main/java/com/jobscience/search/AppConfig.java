dae63261349de69746228441904d81223779439b
flymyao <flymyao@gmail.com>
2015-05-18 09:29:32 +0800

    . update JSS_VERSION = "DROP-064-SNAPSHOT"

--- src/main/java/com/jobscience/search/AppConfig.java
+++ src/main/java/com/jobscience/search/AppConfig.java
@@ -28,7 +28,7 @@ public class AppConfig extends AbstractModule {
     @SuppressWarnings("unused")
     private static Logger log = LoggerFactory.getLogger(AppConfig.class);
 
-	public static String JSS_VERSION = "DROP-063";
+	public static String JSS_VERSION = "DROP-064-SNAPSHOT";
     
     @Override
     protected void configure() {
